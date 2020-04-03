0217cd63a2f13ba40fe512ece17034d4888f1887
Leo Skinner <leoskinner@gmail.com>
2020-04-03 18:35:00 -0300

    Mobile v.0.1

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import logoImg from '../../assets/logo.png';
+import Styles from './styles';
+import styles from './styles';
+export default function Incidents() {
+    return (
+        < View style={styles.container} >
+            <View style={styles.header} >
+
+            </View>
+        </View>
+    );
+}
